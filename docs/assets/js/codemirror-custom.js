13ce637a584d0fab280fcfd50b0435c38ae9b8c8
Doctor <remembersoftwares@gmail.com>
2024-09-08 18:20:33 +0200

    adding managing_agent history page

--- docs/assets/js/codemirror-custom.js
+++ docs/assets/js/codemirror-custom.js
@@ -57,11 +57,41 @@ function initCodeMirrorForOutputTags() {
             showCursorWhenSelecting: false
         });
     });
+}
 
+function initCodeMirrorForJSONTags() {
+
+    const codeTags = document.querySelectorAll('code.language-json'); // Find all <code> with class 'language-python'
+
+    codeTags.forEach(codeTag => {
+        const codeContent = codeTag.innerText.trim(); // Get the content inside the <code> block
+        const preTag = codeTag.parentElement; // Get the parent <pre> element
+
+        // Create a new textarea element
+        const textarea = document.createElement('textarea');
+        const randomId = 'cm-editor-' + Math.random().toString(36).substring(7); // Generate a random ID
+        textarea.id = randomId;
+        textarea.value = codeContent; // Set the value of the textarea to the Python code
+
+        // Insert the textarea after the <pre> tag
+        preTag.insertAdjacentElement('afterend', textarea);
+
+        // Initialize CodeMirror from the newly created textarea
+        CodeMirror.fromTextArea(textarea, {
+            mode: "application/json",  // Plain text mode
+            theme: "monokai",    // Or any theme you prefer
+            lineNumbers: false, // Disable line numbers for text output
+            readOnly: true,     // Make the editor read-only
+            lineWrapping: true, // Enable line wrapping
+            styleActiveLine: false, // Disable active line styling
+            showCursorWhenSelecting: false
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     // Call the function to initialize CodeMirror
     initCodeMirrorForCodeTags();
     initCodeMirrorForOutputTags();
+    initCodeMirrorForJSONTags();
 })
